856f065e9111dadfd97be47dbf020defffb8874f
Nil <nilbiescas3@gmail.com>
2026-05-14 00:30:25 +0200

    u

--- classificationhead/train.py
+++ classificationhead/train.py
@@ -9,6 +9,8 @@ from accelerate import Accelerator
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from optimizers.lozo import LOZOM, LOZO
 
 def parse_args():
