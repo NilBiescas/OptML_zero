8b0fe678f9c6788dfddedef2e480d85447e9a896
Nil <nilbiescas3@gmail.com>
2026-06-04 11:34:41 +0200

    chore: untrack legacy method directories

--- LOZO/data/download_dataset.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Get the username dynamically based on the HF_TOKEN
-HF_USER=$(python -c "from huggingface_hub import HfApi; import os; print(HfApi(token=os.environ.get('HF_TOKEN')).whoami()['name'])")
-
-echo "Downloading dataset from: ${HF_USER}/LM-BFF-datasets"
-
-# Download the dataset (which contains the 'original' folder) into the current directory
-huggingface-cli download --repo-type dataset "${HF_USER}/LM-BFF-datasets" --local-dir . --token "${HF_TOKEN:-}"
--- LOZO/data/download_princeton.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget https://nlp.cs.princeton.edu/projects/lm-bff/datasets.tar
-tar xvf datasets.tar
-mv data original
-rm datasets.tar
