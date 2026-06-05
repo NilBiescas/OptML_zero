71e23584e5cf0810b37a777ecfa278306a0e52b2
Nil <nilbiescas3@gmail.com>
2026-06-05 18:14:56 +0200

    Update

--- submit_pseuzo.sh
+++ submit_pseuzo.sh
@@ -7,7 +7,7 @@ GASPAR="nil"
 
 GPUS=1
 NODE="${NODE:-h100}"
-PROJECT="${PROJECT:-dhlab-${GASPAR}}"
+PROJECT="${PROJECT:-vilab-${GASPAR}}"
 IMAGE="registry.rcp.epfl.ch/course-cs-552/base-vllm:v1"
 
 if [ -f .env ]; then
